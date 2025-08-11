c2d3fdb90318e662aae8a7c1dbd1967c58e1012f
Sayaksome <sayaksome007@gmail.com>
2025-08-12 00:24:39 +0530

    Frontend Done

---
+++ Frontend/src/components/Cards.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Cards = (data) => {
+  return (
+    <>
+      <div className="mt-4 my-3 p-2">
+        <div className="card bg-base-100 w-92 shadow-xl hover:scale-105 duration-200 dark:bg-slate-900 dark:text-white dark:border">
+          <figure>
+            <img src={data.item.image} alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              {data.item.name}
+              <div className="badge badge-secondary">{data.item.category}</div>
+            </h2>
+            <p>{data.item.title}</p>
+            <div className="card-actions justify-between">
+              <div className="badge badge-outline">${data.item.price}</div>
+              <div className="cursor-pointer px-2 py-1 rounder-full border-[2px] badge badge-outline hover:bg-pink-500 hover:text-white duration-200">
+                Buy now
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Cards;
